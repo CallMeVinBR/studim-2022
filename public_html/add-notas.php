642e75fadc97237f091c06cd5adbc9c3131afe0b
Vinicius <165406865+CallMeVinBR@users.noreply.github.com>
2024-07-23 15:53:36 -0300

    correcoes no php

--- public_html/add-notas.php
+++ public_html/add-notas.php
@@ -13,3 +13,4 @@
     $stmt = $mysqli->prepare("INSERT INTO notas (not_al_cod, not_pro_cod, not_cur_cod, not_bi_1, not_bi_2, not_bi_3, not_bi_4) VALUES(?, ?, ?, ?, ?, ?, ?)");
     $stmt->bind_param("iiidddd", $aluno, $prof, $curso, $bi1, $bi2, $bi3, $bi4);
     $stmt->execute();
+    header('Location: ./notas.php');
--- public_html/config-login.php
+++ public_html/config-login.php
@@ -1,7 +1,7 @@
 <?php
 require './adm/conexao.php';
 session_start();
-ob_start();
+
 if (isset($_POST['email'])){
     
     $email = $_POST['email'];
@@ -16,31 +16,36 @@ if (isset($_POST['email'])){
     while ($row = mysqli_fetch_assoc($result1)){
         $senha_bd = $row['al_senha'];
         if(password_verify($senha_input, $senha_bd) and $row['al_email'] == $email){
-            $_SESSION["password"] = $row['al_senha'];
             $_SESSION["email"] = $row['al_email'];
         
             header("Location: ./aluno/aluno.php");
         }
+        else{
+            header("Location: ./index.php?a=err");
+        }
+        
     }
     
     while ($row = mysqli_fetch_assoc($result2)){ 
         $senha_bd = $row['pro_senha'];
          if(password_verify($senha_input, $senha_bd) and $row['pro_email'] == $email){
-            $_SESSION["password"] = $row['pro_senha'];
             $_SESSION["email"] = $row['pro_email'];
              
             header("Location: ./prof/professor.php");
+        }
+        else{
+            header("Location: ./index.php?a=err");
         }
     }
     
     while ($row = mysqli_fetch_assoc($result3)){ 
         if($senha == $row['senha'] and $row['email'] == $email){
-            $_SESSION["password"] = $row['senha'];
             $_SESSION["email"] = $row['email'];
              
-         header("Location: ./adm/menu.php");
+            header("Location: ./adm/menu.php");
+        }
+        else{
+            header("Location: ./index.php?a=err");
         }
     }
 }
-
-ob_end_flush();
